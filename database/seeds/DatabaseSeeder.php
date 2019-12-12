9f989abdca79ff66d20b2f1eebb793868bec77aa
Laytan Laats <Laytanlaats@hotmail.com>
2019-12-12 16:29:30 +0100

    added roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'member']);
+
+        $super = new User();
+        $super->name = 'beheer';
+        $super->email = 'beheer@crazydutchbikers.nl';
+        $super->email_verified_at = now();
+        $super->remember_token = Str::random(10);
+        $super->password = '$2y$12$X1HU74UzvASfEqZkBHYqc.ZRnvNnyILcOFA2EDqLsEDcfAOjb/jPi';
+        $super->assignRole('admin');
+        $super->save();
     }
 }
