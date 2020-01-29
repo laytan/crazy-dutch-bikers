44b6f4c41340de10f020a893a9e51ce8b441bf34
Laytan Laats <Laytanlaats@hotmail.com>
2020-01-29 14:50:50 +0100

    Jquery slim version

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,7 @@ window._ = require('lodash');
 
 try {
     // window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery/dist/jquery.slim');
 
     // Bootstrap
     require('bootstrap/js/dist/alert');
