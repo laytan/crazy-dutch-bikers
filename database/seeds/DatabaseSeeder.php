c2da88407b12ad241df1c4b94efebad2dd4603c2
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-02 01:24:33 +0100

    removed spatie permissions because it was overkill and added language files for nl

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,17 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'super-admin']);
-        Role::create(['name' => 'admin']);
-        Role::create(['name' => 'member']);
-
         $super = new User;
         $super->name = 'beheer';
         $super->email = 'beheer@crazydutchbikers.nl';
         $super->email_verified_at = now();
         $super->remember_token = Str::random(10);
         $super->password = '$2y$12$X1HU74UzvASfEqZkBHYqc.ZRnvNnyILcOFA2EDqLsEDcfAOjb/jPi';
-        $super->assignRole('super-admin');
+        $super->role = 'super-admin';
         $super->save();
     }
 }
