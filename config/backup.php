4ddb852a529212c3e2af029e6e52756d1a4d9407
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-02 22:08:53 +0100

    change mb treshold to 1900mb because dropbox can have up to 2gb for free

--- config/backup.php
+++ config/backup.php
@@ -167,10 +167,10 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['local'],
+            'disks' => ['dropbox'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
-                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
+                \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 1900,
             ],
         ],
 
@@ -229,7 +229,7 @@ return [
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              */
-            'delete_oldest_backups_when_using_more_megabytes_than' => 5000,
+            'delete_oldest_backups_when_using_more_megabytes_than' => 1900,
         ],
     ],
 
