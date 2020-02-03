059ca1e152c86d4d1a98e28b12916324aadb0263
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-03 01:30:06 +0100

    add gallery import script and basic gallery view

---
+++ app/Gallery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    public function pictures() {
+        return $this->hasMany('App\Picture');
+    }
+}
--- design/todo.md
+++ design/todo.md
@@ -3,4 +3,3 @@
 + Change to aire forms
 + add galleries and pictures
 + batch import pictures
-+ Backup creation and sending email when it's done
