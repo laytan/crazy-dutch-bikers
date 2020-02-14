c444e74538cda3cac78ae1c3d4f5b16b0897f535
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-14 19:56:10 +0100

    extract alerts to component and partial

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,6 @@
+<div role="alert" class="alert-dismissible fade show alert alert-{{ $type }}">
+    <p class="mb-0"> {{ $slot }} </p>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
