0855e7bc63929e4065dc71c92f10ac968f0af173
Laytan Laats <laytanlaats@hotmail.com>
2020-02-14 16:39:28 +0100

    homepage initial ideas

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 (() => {
   $(window).on('load', () => {
     copyModalsToFooter();
+    matchHeights();
   });
 
   function copyModalsToFooter() {
@@ -11,6 +12,17 @@ require('./bootstrap');
       const content = modal.cloneNode(true);
       modal.remove();
       $(content).appendTo('footer');
+    });
+  }
+
+  /**
+   * Match the height of data-match elements to the accumalated height of the children of the element to match
+   */
+  function matchHeights() {
+    document.querySelectorAll('[data-match]').forEach(el => {
+      const toMatch = document.querySelector(el.dataset.match);
+      const height = Array.from(toMatch.children).reduce((aggr, el) => aggr + el.scrollHeight, 0);
+      el.style.height = height + 'px';
     });
   }
 })();
--- resources/views/components/nav-button.blade.php
+++ resources/views/components/nav-button.blade.php
@@ -2,6 +2,7 @@
   <a href="{{ $link }}" class="cdb-nav-item d-inline-flex align-items-center">
     <div class="cdb-nav-item__icon p-3">
       <img class="object-fit-cover" src="{{ url('/images/engines/' . $engine . '.png') }}">
+      {{-- <i class="fas fa-calendar-week fa-4x"></i> --}}
     </div>
     <span class="cdb-nav-item__inner h3">{{ $slot }}</span>
   </a>
