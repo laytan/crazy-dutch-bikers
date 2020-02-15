705a3cd2b192a4a6f50ba225c52de3cf237a14be
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-15 01:57:09 +0100

    switch product edit and create to aire forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,26 +3,26 @@
 @section('content')
 <div class="container text-light">
   <h2>Merchandise toevoegen</h2>
-  @include('partials.form-errors')
-  <form action="{{ route('products.store') }}" method="post" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">
-      <label for="title">Titel</label>
-      <input type="text" name="title" id="title" class="form-control" placeholder="Titel">
+  {{ Aire::open()->route('products.store')->multipart()->validate('App\Http\Requests\CreateProductRequest') }}
+  <div class="row">
+    <div class="col-8">
+      {{ Aire::input('title', 'Titel') }}
     </div>
-    <div class="form-group">
-      <label for="description">Beschrijving</label>
-      <textarea type="text" name="description" id="description" class="form-control" placeholder="Beschrijving"></textarea>
+    <div class="col-4">
+      {{ Aire::number('price', 'Prijs in centen') }}
     </div>
-    <div class="form-group">
-      <label for="price">Prijs in centen</label>
-      <input type="number" name="price" id="price" class="form-control" placeholder="Prijs">
+  </div>
+  <div class="row mb-4">
+    <div class="col-8">
+      {{ Aire::textArea('description', 'Beschrijving')->rows(10) }}
     </div>
-    <div class="form-group">
-      <label for="product_picture">Productfoto</label>
-      <input type="file" name="product_picture" id="product_picture" class="form-control-file">
+    <div class="col-4" style="height: 335px;">
+      @component('components.image-upload', ['id' => 'product_picture', 'name' => 'product_picture', 'initSelf' => true])
+      Productfoto kiezen
+      @endcomponent
     </div>
-    <input class="btn btn-primary" type="submit" value="Aanmaken">
-  </form>
+  </div>
+  {{ Aire::submit('Aanmaken') }}
+  {{ Aire::close() }}
 </div>
 @endsection
