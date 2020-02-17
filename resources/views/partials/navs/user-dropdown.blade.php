8fccf5f550de2b1a182ba976fe2dcdf1fcc13c74
Laytan Laats <laytanlaats@hotmail.com>
2020-02-17 16:27:26 +0100

    use data-submit on all onclick submit's and preventdefault in data-submit handler

--- resources/views/partials/navs/user-dropdown.blade.php
+++ resources/views/partials/navs/user-dropdown.blade.php
@@ -2,11 +2,9 @@
 	<a href="{{ route('users.edit', ['user' => Auth::user()->id]) }}" class="dropdown-item">Profiel</a>
 	@include('partials.change-password')
 	<a
-		class="dropdown-item"
-		href="#"
-		onclick="event.preventDefault();
-		document.getElementById('logout-form').submit();"
-	>
+	class="dropdown-item"
+	href="#"
+	data-submit="#logout-form">
 		Uitloggen
 	</a>
 	{{ Aire::open()->route('logout')->id('logout-form')->class('d-none') }}
