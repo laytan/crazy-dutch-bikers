58494a38f706c895e45440d49b3c5772ce35c738
Laytan Laats <laytanlaats@hotmail.com>
2020-02-17 15:53:58 +0100

    new navigation mobile version

---
+++ resources/views/partials/navs/user-dropdown.blade.php
@@ -0,0 +1,14 @@
+@component('components.dropdown', ['id' => 'user-dropdown', 'title' => Auth::user()->name, 'right' => false])
+	<a href="{{ route('users.edit', ['user' => Auth::user()->id]) }}" class="dropdown-item">Profiel</a>
+	@include('partials.change-password')
+	<a
+		class="dropdown-item"
+		href="#"
+		onclick="event.preventDefault();
+		document.getElementById('logout-form').submit();"
+	>
+		Uitloggen
+	</a>
+	{{ Aire::open()->route('logout')->id('logout-form')->class('d-none') }}
+	{{ Aire::close() }}
+@endcomponent
