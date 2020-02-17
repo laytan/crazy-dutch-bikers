abcb21be7ae0c5ed8fbb36c3dcb36c0513212157
Laytan Laats <laytanlaats@hotmail.com>
2020-02-17 15:53:58 +0100

    new navigation mobile version

--- public/js/app.js
+++ public/js/app.js
@@ -35943,8 +35943,8 @@ exports.default = Cart;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Laytan\Documents\Projecten\crazy-dutch-bikers\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Laytan\Documents\Projecten\crazy-dutch-bikers\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Laytan\Desktop\projecten\crazy-dutch-bikers\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Laytan\Desktop\projecten\crazy-dutch-bikers\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/partials/navs/admin.blade.php
@@ -0,0 +1,26 @@
+@component('components.dropdown', ['id' => 'members-dropdown', 'title' => 'Leden'])
+	<a href="{{ route('users.create') }}" class="dropdown-item">Leden toevoegen</a>
+	<a href="{{ route('users.index') }}" class="dropdown-item">Leden overzicht</a>
+@endcomponent
+@component('components.dropdown', ['id' => 'events-dropdown', 'title' => 'Evenementen'])
+	<a href="{{ route('events.create') }}" class="dropdown-item">Evenementen toevoegen</a>
+	<a href="{{ route('events.index') }}" class="dropdown-item">Evenementen overzicht</a>
+@endcomponent
+@component('components.dropdown', ['id' => 'merchandise-dropdown', 'title' => 'Merchandise'])
+	<a href="{{ route('products.create') }}" class="dropdown-item">Merchandise toevoegen</a>
+	<a href="{{ route('products.index') }}" class="dropdown-item">Merchandise overzicht</a>
+@endcomponent
+@component('components.dropdown', ['id' => 'gallery-dropdown', 'title' => 'Gallerij'])
+	<a href="{{ route('galleries.create') }}" class="dropdown-item">Gallerij toevoegen</a>
+	<a href="#" class="dropdown-item">Gallerijen overzicht</a>
+	<a href="#" class="dropdown-item">Foto's toevoegen</a>
+	<a href="#" class="dropdown-item">Foto's overzicht</a>
+@endcomponent
+<li class="nav-item">
+	<div class="d-flex align-items-center">
+		<a href="{{ route('orders.index') }}" class="nav-link d-flex align-items-center">
+			Bestellingen<span class="badge badge-primary ml-2">{{ orderAmt() }}</span>
+		</a>
+	</div>
+</li>
+@include('partials.navs.user-dropdown')
