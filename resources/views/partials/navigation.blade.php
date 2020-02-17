90de189d72c74385bfd689e54d61ababdc5d6442
Laytan Laats <laytanlaats@hotmail.com>
2020-02-17 15:53:58 +0100

    new navigation mobile version

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,80 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-dark navigation sticky-top">
-    <a class="navbar-brand h1 m-0 mr-lg-5" href="{{ route('index') }}">
-        Cra<span class="navbar-brand__z">Z</span>y Dutch Bikers
-    </a>
-    <button
-    class="navbar-toggler border-0"
-    type="button"
-    data-toggle="collapse"
-    data-target=".dual-collapse"
-    aria-controls="navigation"
-    aria-expanded="false" 
-    aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-    </button>
-    <div id="navigation" class="navbar-collapse collapse w-100 order-1 order-lg-0 dual-collapse">
-        <ul class="navbar-nav mr-auto">
-            @can('manage')
-                @component('components.dropdown', ['id' => 'members-dropdown', 'title' => 'Leden'])
-                    <a href="{{ route('users.create') }}" class="dropdown-item">Leden toevoegen</a>
-                    <a href="{{ route('users.index') }}" class="dropdown-item">Leden overzicht</a>
-                @endcomponent
-                @component('components.dropdown', ['id' => 'events-dropdown', 'title' => 'Evenementen'])
-                    <a href="{{ route('events.create') }}" class="dropdown-item">Evenementen toevoegen</a>
-                    <a href="{{ route('events.index') }}" class="dropdown-item">Evenementen overzicht</a>
-                @endcomponent
-                @component('components.dropdown', ['id' => 'merchandise-dropdown', 'title' => 'Merchandise'])
-                    <a href="{{ route('products.create') }}" class="dropdown-item">Merchandise toevoegen</a>
-                    <a href="{{ route('products.index') }}" class="dropdown-item">Merchandise overzicht</a>
-                @endcomponent
-                @component('components.dropdown', ['id' => 'gallery-dropdown', 'title' => 'Gallerij'])
-                    <a href="{{ route('galleries.create') }}" class="dropdown-item">Gallerij toevoegen</a>
-                    <a href="#" class="dropdown-item">Gallerijen overzicht</a>
-                    <a href="#" class="dropdown-item">Foto's toevoegen</a>
-                    <a href="#" class="dropdown-item">Foto's overzicht</a>
-                @endcomponent
-                <li class="nav-item">
-                    <div class="d-flex align-items-center">
-                        <a href="{{ route('orders.index') }}" class="nav-link d-flex align-items-center">
-                            Bestellingen<span class="badge badge-primary ml-2">{{ orderAmt() }}</span>
-                        </a>
-                    </div>
-                </li>
-            @else
-                <li class="nav-item">
-                    <a href="{{ route('events.index') }}" class="nav-link text-decoration-none">Evenementen</a>
-                </li>
-                <li class="nav-item">
-                    <a dusk="merchandise-btn" href="{{ route('products.index') }}" class="nav-link text-decoration-none">Merchandise</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link text-decoration-none">Gallerij</a>
-                </li>
-            @endcan
-        </ul>
+<nav class="navbar navbar-expand-lg fixed-bottom navigation p-0">
+    <div id="navigation" class="navbar-collapse collapse">
+        <div class="w-100 d-flex justify-content-end">
+            <ul class="navbar-nav px-3 bg-cdbb d-inline-block">
+                @guest
+                    @include('partials.navs.guest')
+                @else
+                    @can('manage')
+                        @include('partials.navs.admin')
+                    @else
+                        @include('partials.navs.member')
+                    @endcan
+                @endguest
+            </ul>
+        </div>
     </div>
-    <div class="navbar-collapse collapse w-100 order-2 dual-collapse">
-        <ul class="navbar-nav ml-lg-auto justify-content-end">
-            @guest
-                <li class="nav-item">
-                    @include('partials.login')
-                </li>
-            @else
-                @component('components.dropdown', ['id' => 'user-dropdown', 'title' => Auth::user()->name, 'right' => true])
-                    <a href="{{ route('users.edit', ['user' => Auth::user()->id]) }}" class="dropdown-item">Profiel</a>
-                    @include('partials.change-password')
-                    <a
-                        class="dropdown-item"
-                        href="#"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();"
-                    >
-                        Uitloggen
-                    </a>
-                    {{ Aire::open()->route('logout')->id('logout-form')->class('d-none') }}
-                    {{ Aire::close() }}
-                @endcomponent
-            @endguest
-        </ul>
+    <div class="bg-cdbb w-100 navbar-text d-flex justify-content-between pl-3 pr-1">
+        <a href="{{ url('/') }}" class="navbar-brand navigation__logo">
+            <img src="{{ url('/images/cdb-logo.png') }}" alt="Crazy Dutch Bikers logo">
+        </a>
+        <button class="btn btn-primary btn-sm align-self-center">
+            <i class="fas fa-user-lock"></i> Members only
+        </button>
+        <button
+        class="navbar-toggler border-0 collapsed navigation-toggler"
+        aria-expanded="false"
+        aria-controls="#navigation"
+        data-toggle="collapse"
+        data-target="#navigation"
+        type="button">
+            <span class="navigation-toggler__bar"></span>
+            <span class="navigation-toggler__bar"></span>
+            <span class="navigation-toggler__bar"></span>
+        </button>
     </div>
 </nav>
