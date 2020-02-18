6b3b1165f4c447e4c85f28b00c73690c932e7637
Laytan Laats <laytanlaats@hotmail.com>
2020-02-18 15:55:05 +0100

    adding pictures through model method instead of in controller

--- app/Gallery.php
+++ app/Gallery.php
@@ -3,11 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
 
 class Gallery extends Model
 {
     public function pictures()
     {
         return $this->hasMany('App\Picture');
+    }
+
+    /**
+     * Add pictures to the gallery
+     * @param UploadedFile[] $pictures - Pictures to add.
+     */
+    public function addPictures(array $pictures)
+    {
+        return array_map(fn($picture) => $this->addPicture($picture), $pictures);
+    }
+
+    /**
+     * Add a picture to the gallery
+     * @param UploadedFile $picture - Picture to add.
+     */
+    public function addPicture(UploadedFile $picture): Picture
+    {
+        $pictureUrl = $picture->store("galleries/$this->title", ['disk' => 'private']);
+        $picture = new Picture();
+        $picture->gallery_id = $this->id;
+        $picture->url = $pictureUrl;
+        $picture->save();
+        return $picture;
     }
 }
