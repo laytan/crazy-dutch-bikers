9644cea9dd5160587349f56f03ddfb34fb638b70
Laytan Laats <laytanlaats@hotmail.com>
2020-02-18 11:50:43 +0100

    audio logic, move types to dev dependecies and remove double bootstrap require

--- resources/js/audio-theme.ts
+++ resources/js/audio-theme.ts
@@ -1,88 +1,239 @@
+enum Status {
+  Muted = "MUTED",
+  Paused = "PAUSED",
+  Playing = "PLAYING",
+  Ended = "ENDED",
+  Loading = "LOADING",
+}
+
+interface PartialState {
+  status?: Status,
+  currentTime?: number,
+}
+
+interface State {
+  status: Status,
+  currentTime: number,
+}
+
 export default class AudioTheme {
   
   private audioElement: HTMLAudioElement;
-  private playButton: Element|null;
-  private progress: HTMLProgressElement;
-  private currentTime: Element|null;
-  private state = 'start'; // start, pause, playing, ended
+  private state: State;
+  private duration: number;
   
-  constructor (private wrapper: Element, private hint: Element) {
+  constructor (private wrapper: Element, private songName: string) {
     this.audioElement = wrapper.getElementsByTagName('audio')[0];
-    this.playButton = wrapper.querySelector('.js-audio-player__play');
-    this.progress = wrapper.getElementsByTagName('progress')[0];
-    this.currentTime = wrapper.querySelector('.js-audio-player__current');
-    
-    const duration = wrapper.querySelector('.js-audio-player__duration');
-    if (duration) {
-      duration.textContent = AudioTheme.parseTime(this.audioElement.duration);
-    }
 
-    this.playButton?.addEventListener('click', this.play.bind(this));
-    this.audioElement.addEventListener('ended', this.ended.bind(this));
+    this.state = {
+      status: Status.Loading,
+      currentTime: 0,
+    };
 
-    this.tryAutoplay();
+    this.duration = this.audioElement.duration;
 
-    setInterval(this.updateProgress.bind(this), 1000);
+    this.start();
   }
 
-  // TODO: Clean up
+  /**
+   * Turns 80 into 01:20 etc.
+   * @param val number of seconds
+   */
   static parseTime(val: number): string {
     const string = val.toString();
-    const numberOfSeconds = parseInt(string, 10); // don't forget the second param
+    const numberOfSeconds = parseInt(string, 10);
     const hours   = Math.floor(numberOfSeconds / 3600);
     const minutes = Math.floor((numberOfSeconds - (hours * 3600)) / 60);
     const seconds = numberOfSeconds - (hours * 3600) - (minutes * 60);
 
     let stringMinutes = minutes.toString();
     let stringSeconds = seconds.toString();
-    if (minutes < 10) {stringMinutes = "0"+minutes;}
-    if (seconds < 10) {stringSeconds = "0"+seconds;}
-    return stringMinutes+':'+stringSeconds;
+    if (minutes < 10) {
+      stringMinutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      stringSeconds = `0${seconds}`;
+    }
+    return `${stringMinutes}:${stringSeconds}`;
   }
 
-  private tryAutoplay() {
+  /**
+   * Create objects for all data-audio-player=""
+   */
+  static initialize() {
+    const audioPlayers = document.querySelectorAll('[data-audio-player=""');
+    audioPlayers.forEach(player => {
+      if(!(player instanceof HTMLDivElement)) {
+        console.error('Audio player should be a div element');
+        return;
+      }
+      const songName = player.dataset.songName;
+      if(!songName) {
+        console.error('Audio element needs a data-song-name attribute');
+        return;
+      }
+      new AudioTheme(player, songName);
+    });
+  }
+
+  /**
+   * Try to autoplay the audio, if it didn't work mute and play it that way
+   */
+  private start() {
+    this.audioElement.addEventListener('ended', () => this.setState({ status: Status.Ended }));
+
     this.audioElement.play()
       .then(() => {
-        this.play();
+        // Autoplay worked
+        this.setState({
+          status: Status.Playing,
+        });
       })
       .catch(() => {
-        this.hintPlay();
+        // Autoplay did not work, mute audio and play again
+        this.audioElement.muted = true;
+        this.audioElement.play();
+        this.setState({
+          status: Status.Muted,
+        });
+      }).finally(() => {
+        setInterval(this.updateTime.bind(this), 1000);
       });
   }
 
-  private hintPlay() {
-    this.hint.classList.add('d-block');
+  /**
+   * Update time when we have the audioelement actually playing
+   */
+  private updateTime() {
+    if(this.state.status === Status.Playing || this.state.status === Status.Muted) {
+      this.setState({ currentTime: this.audioElement.currentTime });
+    }
   }
 
-  // TODO: setState function so state is in one place
-  private play() {
-    switch(this.state) {
-      case 'playing':
-        this.audioElement.pause();
-        this.state = 'pause';
+  /**
+   * Change the state and render with that state
+   * @param state - The state you wish to change
+   */
+  private setState(state: PartialState) {
+    this.state = {
+      status: state.status || this.state.status,
+      currentTime: state.currentTime || this.state.currentTime,
+    };
+    this.render();
+  }
+
+  /**
+   * Handle click on the current icon
+   */
+  private onClickIcon() {
+    switch(this.state.status) {
+      case Status.Muted:
+        this.audioElement.muted = false;
+        this.setState({ status: Status.Playing });
         break;
-      case 'ended':
+      case Status.Ended:
         this.audioElement.currentTime = 0;
         this.audioElement.play();
-        this.state = 'playing';
+        this.setState({ status: Status.Playing });
         break;
-      default:
+      case Status.Loading:
+        // Try starting again
+        this.start();
+        break;
+      case Status.Paused:
         this.audioElement.play();
-        this.state = 'playing';
+        this.setState({ status: Status.Playing });
+        break;
+      case Status.Playing:
+        this.audioElement.pause();
+        this.setState({ status: Status.Paused });
         break;
     }
   }
 
-  private ended() {
-    this.state = 'ended';
+  /**
+   * On click progress bar check the click percent and adjust current time
+   * @param e progress bar click event
+   */
+  private seek(e: MouseEvent) {
+    const target = e.target as HTMLProgressElement;
+    const percent = e.offsetX / target.offsetWidth;
+    this.audioElement.currentTime = percent * this.duration;
+    this.setState({ currentTime: this.audioElement.currentTime });
+  }
+  
+  /**
+   * Return the progress percentage that the progress element takes
+   */
+  private getProgress(): string {
+    return (this.state.currentTime / this.duration).toString();
   }
 
-  // TODO: rename
-  private updateProgress() {
-    this.wrapper.setAttribute('data-state', this.state);
-    this.progress.value = (this.audioElement.currentTime / this.audioElement.duration);
-    if(this.currentTime) {
-      this.currentTime.textContent = AudioTheme.parseTime(this.audioElement.currentTime);
+  /**
+   * Return an icon based on the status
+   */
+  private getIcon(): string {
+    switch(this.state.status) {
+      case Status.Muted:
+        return '<i class="fas fa-volume-up"></i>';
+      case Status.Paused:
+        return '<i class="fas fa-play"></i>';
+      case Status.Playing:
+        return '<i class="fas fa-pause"></i>';
+      case Status.Ended:
+        return '<i class="fas fa-redo"></i>';
+      case Status.Loading:
+        return '<i class="fas fa-spinner fa-spin"></i>'
     }
+  }
+
+  /**
+   * Let the user know that the audio is muted because autoplay is blocked
+   */
+  private getMutedNotice() {
+    return `
+    <div>
+      We have muted your audio because playing our theme song is blocked by your browser. You can unmute it here.
+    </div>
+    `;
+  }
+
+  /**
+   * Render the element with the current state
+   */
+  private render() {
+    const renderIn = this.wrapper.querySelector('.js-render');
+    if(!renderIn) return;
+    renderIn.innerHTML = `
+    <div class="js-audio-player audio-player bg-cdbg-opaque border-cdbb fixed-top py-1" data-bpm="72" data-state="${this.state.status}">
+      <div class="d-flex justify-content-around align-items-center">  
+        <div class="audio-player__play">
+          ${this.getIcon()}
+        </div>
+        <div class="d-flex align-items-center flex-column pr-3 pl-2">
+          <p class="mb-0 lead">
+            ${this.songName}
+          </p>
+          <progress value="${this.getProgress()}" max="1" class="mw-100 cursor-pointer"></progress>
+          <div class="d-flex justify-content-between w-100">
+            <small class="js-audio-player__current">
+              ${AudioTheme.parseTime(this.state.currentTime)}
+            </small>
+            <small class="js-audio-player__duration">
+              ${AudioTheme.parseTime(this.duration)}
+            </small>
+          </div>
+        </div>
+      </div>
+    </div>
+    ${ this.state.status === Status.Muted ? this.getMutedNotice() : '' }
+    `;
+
+    // Set up event listeners
+    const icon = renderIn.querySelector('.audio-player__play i');
+    icon?.addEventListener('click', this.onClickIcon.bind(this));
+
+    const progress = renderIn.querySelector('progress');
+    progress?.addEventListener('click', this.seek.bind(this));
   }
 }
