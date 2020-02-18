8fcd6ca266650b8466c0880fb14fe7726695b484
Laytan Laats <laytanlaats@hotmail.com>
2020-02-18 14:25:57 +0100

    fix not able to migrate bcs of index length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 use Route;
 
@@ -29,5 +30,7 @@ class AppServiceProvider extends ServiceProvider
             'create' => 'aanmaken',
             'edit'   => 'bewerken',
         ]);
+
+        Schema::defaultStringLength(191);
     }
 }
