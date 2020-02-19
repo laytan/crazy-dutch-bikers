becbf27290a934c9c4038516913fda9e76184502
Laytan Laats <laytanlaats@hotmail.com>
2020-02-19 16:25:51 +0100

    responsive nav and audio player, audio player progress styling and hint handling, add upcoming event section on home

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg fixed-bottom navigation p-0">
-    <div id="navigation" class="navbar-collapse collapse">
+<nav class="navbar navbar-expand-lg fixed-bottom lg-sticky-top navigation p-0 bg-lg-cdbb">
+    <div id="navigation" class="navbar-collapse collapse order-lg-2">
         <div class="w-100 d-flex justify-content-end">
-            <ul class="navbar-nav px-3 bg-cdbb d-inline-block">
+            <ul class="navbar-nav px-3 bg-cdbb d-inline-block w-100 d-lg-flex align-items-center">
                 @guest
                     @include('partials.navs.guest')
                 @else
@@ -12,13 +12,19 @@
                     @endcan
                 @endguest
             </ul>
+            @yield('nav-end')
         </div>
     </div>
-    <div class="bg-cdbb w-100 navbar-text d-flex justify-content-between pl-3 pr-1">
-        <a href="{{ url('/') }}" class="navbar-brand navigation__logo">
-            <img src="{{ url('/images/cdb-logo.png') }}" alt="Crazy Dutch Bikers logo">
+    <div class="bg-cdbb w-100 navbar-text d-flex justify-content-between pl-3 pr-1 w-lg-auto order-lg-1">
+        <a href="{{ url('/') }}" class="navbar-brand">
+            <div class="navigation__logo d-inline-block">
+                <img src="{{ url('/images/cdb-logo.png') }}" alt="Crazy Dutch Bikers logo">
+            </div>
+            <h1 class="d-none d-xl-inline-block brand-text mb-0">
+                Cra<span class="brand-text__z">Z</span>y Dutch Bikers
+            </h1>
         </a>
-        <button class="btn btn-primary btn-sm align-self-center" data-toggle="modal" data-target="#login">
+        <button class="btn btn-primary btn-sm align-self-center d-lg-none" data-toggle="modal" data-target="#login">
             <i class="fas fa-user-lock"></i> Members only
         </button>
         <button
