0b8180965d6bf17825212234398181511517316b
Laytan Laats <laytanlaats@hotmail.com>
2020-02-19 10:30:41 +0100

    clean up routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 // All auth routes except registration, reset and verify
 Auth::routes([
@@ -23,36 +23,28 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-// Leden
 Route::get('/leden', 'UserController@index')->name('users.index');
 Route::delete('/leden/{user}', 'UserController@destroy')->name('users.destroy');
 Route::get('/leden/{user}/bewerken', 'UserController@edit')->name('users.edit');
 Route::patch('/leden/{user}/bewerken', 'UserController@update')->name('users.update');
-Route::post('/leden/activeer', 'UserController@activate')->name('users.activate');
 Route::get('/leden/aanmaken', 'UserController@create')->name('users.create');
 Route::post('/leden', 'UserController@store')->name('users.store');
-Route::get('/storage/profile-pictures/{profile_picture}', 'UserController@picture');
 
-// Passwords
-Route::get('/change-password', 'ChangePasswordController@index')->name('change-password-index');
-Route::post('/change-password', 'ChangePasswordController@ChangePassword')->name('change-password');
+Route::get('/change-password', 'ChangePasswordController@index');
+Route::patch('/change-password', 'ChangePasswordController@update')->name('change-password');
 
-// Products
 Route::get('/merchandise', 'ProductController@index')->name('products.index');
 Route::get('/merchandise/aanmaken', 'ProductController@create')->name('products.create');
 Route::post('/merchandise', 'ProductController@store')->name('products.store');
 Route::get('/merchandise/{product}/bewerken', 'ProductController@edit')->name('products.edit');
 Route::patch('/merchandise/{product}', 'ProductController@update')->name('products.update');
 Route::delete('/merchandise/{product}', 'ProductController@destroy')->name('products.destroy');
-Route::get('/storage/product-pictures/{product_picture}', 'ProductController@picture');
 
-// Orders
 Route::post('/bestellingen', 'OrderController@store')->name('orders.store');
 Route::get('/bestellingen', 'OrderController@index')->name('orders.index');
 Route::get('/bestellingen/{order}', 'OrderController@show')->name('orders.show');
 Route::patch('/bestellingen/{order}', 'OrderController@update')->name('orders.update');
 
-// Events
 Route::get('/evenementen', 'EventController@index')->name('events.index');
 Route::get('/evenementen/aanmaken', 'EventController@create')->name('events.create');
 Route::post('/evenementen', 'EventController@store')->name('events.store');
@@ -63,20 +55,9 @@ Route::post('/gallerij/aanmaken', 'GalleryController@store')->name('galleries.st
 Route::get('/gallerij/{gallery}', 'GalleryController@show')->name('galleries.show');
 Route::patch('/gallerij/{gallery}', 'GalleryController@update')->name('galleries.update');
 Route::delete('/gallerij/{gallery}', 'GalleryController@destroy')->name('galleries.destroy');
-Route::get('/storage/galleries/{gallery}/{picture}', 'GalleryController@picture');
 
-// Route::group(['middleware' => ['auth']], function () {
-//     Route::get('/storage/product-pictures/{picture}', function ($file) {
-//      //This method will look for the file and get it from drive
-//     $path = storage_path('app/uploads/product-pictures/' . $file);
-//     try {
-//         $file = File::get($path);
-//         $type = File::mimeType($path);
-//         $response = Response::make($file, 200);
-//         $response->header("Content-Type", $type);
-//         return $response;
-//     } catch (FileNotFoundException $exception) {
-//         abort(404);
-//     }
-//     });
-// });
+// TODO: Remove picture implementation because filenames are obscufated anyway
+// and this implementation checks auth and other conditions which is slow
+Route::get('/storage/profile-pictures/{profile_picture}', 'UserController@picture');
+Route::get('/storage/galleries/{gallery}/{picture}', 'GalleryController@picture');
+Route::get('/storage/product-pictures/{product_picture}', 'ProductController@picture');
