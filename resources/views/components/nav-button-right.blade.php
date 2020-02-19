26a2e1b53e8d47b7280f7b8b2c1abeda226f3795
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-20 00:46:29 +0100

    more work on responsiveness (mainly the homepage above nav)

--- resources/views/components/nav-button-right.blade.php
+++ resources/views/components/nav-button-right.blade.php
@@ -1,9 +1,9 @@
 <li class="mb-md-3">
-  <a href="{{ $link }}" class="cdb-nav-item d-inline-flex align-items-center">
+  <a href="{{ $link }}" class="cdb-nav-item cdb-nav-item--right d-inline-flex align-items-center">
+    <span class="cdb-nav-item__inner h4">{{ $slot }}</span>
     <div class="cdb-nav-item__icon p-3">
       <img class="object-fit-cover" src="{{ url('/images/engines/' . $engine . '.png') }}">
       {{-- <i class="fas fa-calendar-week fa-4x"></i> --}}
     </div>
-    <span class="cdb-nav-item__inner h4">{{ $slot }}</span>
   </a>
 </li>
