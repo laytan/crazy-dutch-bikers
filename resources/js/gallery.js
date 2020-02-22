2499b9aed2e3d2975c5308edee4bba7084915a8b
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-22 04:27:05 +0100

    api tokens, gallery upload multiple images start

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import ImageUpload from './components/ImageUpload';
+import GalleryUpload from './components/GalleryUpload';
 
 const app = new Vue({
   el: '#vue',
   components: {
     ImageUpload,
+    GalleryUpload,
   },
 });
