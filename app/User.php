1c17af085f56ea67cfe07ec1f933f11c5526e7e4
Laytan Laats <Laytanlaats@hotmail.com>
2020-02-23 00:39:06 +0100

    add api token on user create

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'description', 'profile_picture'
+        'name', 'email', 'password', 'description', 'profile_picture', 'api_token'
     ];
 
     /**
