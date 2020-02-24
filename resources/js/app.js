23f38cd41d0eccbcf30b3b7f34ac71b931b31c69
Laytan Laats <laytanlaats@hotmail.com>
2020-02-24 17:04:30 +0100

    add eslint and start on using it

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,73 +1,41 @@
-require('./bootstrap');
+/* global $ */
+
 import 'bootstrap';
 import LazyLoad from 'vanilla-lazyload';
-import Cart from './cart';
+import Cart from './cart.ts';
+import AudioTheme from './audio-theme.ts';
+import ImageUpload from './image-upload.ts';
+import ImagesUpload from './images-upload.ts';
+
+require('./bootstrap');
+
 window.Cart = Cart;
-import AudioTheme from './audio-theme';
-import ImageUpload from './image-upload';
-import ImagesUpload from './images-upload';
 
 (() => {
-  $(window).on('load', () => {
-    copyModalsToFooter();
-    setupLazyCarousels();
-
-    AudioTheme.initialize();
-    ImageUpload.initialize();
-    ImagesUpload.initialize();
-
-    new LazyLoad({
-      elements_selector: 'img.lazy'
-    });
-
-    // Handle fullscreening gallery images
-    $('.gallery-grid__image-wrap').on('click', (e) => {
-      const wrap =  $(e.target).parents('.gallery-grid__image-wrap');
-      wrap.toggleClass('gallery-grid__image-wrap--full');
-    });
-    matchHeights();
-    pulsateLogo();
-
-    console.log($('[data-submit]'));
-    // data-submit submits the form with that selector on click
-    $('[data-submit]').click(e => {
-      e.preventDefault();
-      console.log(e);
-      document.querySelector(e.target.dataset.submit).submit();
-    });
-
-    // Set padding bottom on footer when the nav is fixed to the bottom
-    footerPadding();
-    $(window).on('resize', footerPadding);
-});
-
-/**
- * Set footer padding bottom to the nav's height when the window width is less than 992px (bootstrap large breakpoint)
- */
-function footerPadding() {
-    if($(window).width() < 992) {
-        const navHeight = $('.navigation').height();
-        $('footer').css('padding-bottom', `${navHeight}px`);
+  /**
+  * Set footer padding bottom to the nav's height
+  * when the window width is less than 992px (bootstrap large breakpoint)
+  */
+  function footerPadding() {
+    if ($(window).width() < 992) {
+      const navHeight = $('.navigation').height();
+      $('footer').css('padding-bottom', `${navHeight}px`);
     } else {
-        $('footer').css('padding-bottom', '0px');
+      $('footer').css('padding-bottom', '0px');
     }
   }
 
   /**
-   * Puts all modals on the page into the footer, so we can initialize them everywhere and the z-index will works
+   * Puts all modals on the page into the footer,
+   * so we can initialize them everywhere and the z-index will works
    */
   function copyModalsToFooter() {
     const modals = document.querySelectorAll('.modal');
     const footer = document.querySelector('.footer');
-    modals.forEach(modal => {
-        console.log(modal);
-        const modalContent = modal.cloneNode(true);
-        const parent = modal.parentNode;
-        console.log(parent);
-        modal.parentNode.removeChild(modal);
-        console.log(parent);
-        footer.appendChild(modalContent);
-        console.log(footer);
+    modals.forEach((modal) => {
+      const modalContent = modal.cloneNode(true);
+      modal.parentNode.removeChild(modal);
+      footer.appendChild(modalContent);
     });
   }
 
@@ -75,40 +43,29 @@ function footerPadding() {
    * On slide it will load in the next carousel item's image
    */
   function setupLazyCarousels() {
-    $('.carousel.lazy').on('slide.bs.carousel', function(ev) {
+    $('.carousel.lazy').on('slide.bs.carousel', (ev) => {
       const curr = ev.relatedTarget.id;
       const currParts = curr.split('-');
       const next = $(`#${currParts[0]}-${currParts[1]}-${currParts[2]}-${Number(currParts[3]) + 1}`).find('[data-src]');
-      if(next) {
-      next.attr('src', next.data('src'));
-      next.removeAttr('data-src');
+      if (next) {
+        next.attr('src', next.data('src'));
+        next.removeAttr('data-src');
       }
     });
   }
 
   /**
-   * Match the height of data-match elements to the accumalated height of the children of the element to match
+   * Match the height of data-match elements to the
+   * accumalated height of the children of the element to match
    */
   function matchHeights() {
-    document.querySelectorAll('[data-match]').forEach(el => {
+    document.querySelectorAll('[data-match]').forEach((el) => {
       const toMatch = document.querySelector(el.dataset.match);
-      const height = Array.from(toMatch.children).reduce((aggr, el) => aggr + el.scrollHeight, 0);
-      el.style.height = height + 'px';
+      const height = Array.from(toMatch.children).reduce(
+        (aggr, child) => aggr + child.scrollHeight, 0,
+      );
+      el.style.height = `${height}px`;
     });
-  }
-
-  function pulsateLogo() {
-    const audio = document.querySelector('.js-audio-theme');
-    const logo = document.querySelector('.js-logo');
-    if(audio && logo) {
-      const pulse = (60 / Number(audio.dataset.bpm)) * 1000;
-
-      // audio.play();
-      // audio.classList.add('playing');
-
-      pulsing(logo, pulse);
-      setInterval(function() { pulsing(logo, pulse) }, pulse);
-    }
   }
 
   function pulsing(logo, pulse) {
@@ -118,4 +75,49 @@ function footerPadding() {
       logo.classList.remove('pulse');
     }, pulse - 100);
   }
+
+  function pulsateLogo() {
+    const audio = document.querySelector('.js-audio-theme');
+    const logo = document.querySelector('.js-logo');
+    if (audio && logo) {
+      const pulse = (60 / Number(audio.dataset.bpm)) * 1000;
+
+      // audio.play();
+      // audio.classList.add('playing');
+
+      pulsing(logo, pulse);
+      setInterval(() => { pulsing(logo, pulse); }, pulse);
+    }
+  }
+
+  $(window).on('load', () => {
+    copyModalsToFooter();
+    setupLazyCarousels();
+
+    AudioTheme.initialize();
+    ImageUpload.initialize();
+    ImagesUpload.initialize();
+
+    new LazyLoad({
+      elements_selector: 'img.lazy',
+    });
+
+    // Handle fullscreening gallery images
+    $('.gallery-grid__image-wrap').on('click', (e) => {
+      const wrap = $(e.target).parents('.gallery-grid__image-wrap');
+      wrap.toggleClass('gallery-grid__image-wrap--full');
+    });
+    matchHeights();
+    pulsateLogo();
+
+    // data-submit submits the form with that selector on click
+    $('[data-submit]').click((e) => {
+      e.preventDefault();
+      document.querySelector(e.target.dataset.submit).submit();
+    });
+
+    // Set padding bottom on footer when the nav is fixed to the bottom
+    footerPadding();
+    $(window).on('resize', footerPadding);
+  });
 })();
