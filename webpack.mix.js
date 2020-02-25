0625813d4194274566a2a4370a410ce39fb216c9
Laytan Laats <laytanlaats@hotmail.com>
2020-02-25 17:00:32 +0100

    start on featured gallery

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,10 @@ mix.options({ processCssUrls: false });
 mix.js('resources/js/app.js', 'public/js')
   .js('resources/js/gallery.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
-  .purgeCss()
+  .purgeCss({
+    whitelist: ['modal-backdrop', 'text-cdblg', 'custom-checkbox'],
+    whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /form/, /alert/, /carousel/, /item/, /custom-control/],
+  })
   .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
   .webpackConfig({
     module: {
