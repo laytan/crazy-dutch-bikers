91278db40ac55f78a61bbac5e47a3a4a6cfb48ce
Laytan Laats <laytanlaats@hotmail.com>
2020-02-27 16:19:49 +0100

    add privacy/avg and disclaimer

---
+++ resources/views/legal/privacy.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  @component('components.title', ['icon' => 'fas fa-user-secret'])
+  Privacy Beleid
+  @endcomponent
+  <h3 class="h5">Contactgegevens</h3>
+  <p>
+    <div class="d-table-row">
+      <span class="d-table-cell">Website</span>
+      <a class="d-table-cell" href="https://crazydutchbikers.nl">https://crazydutchbikers.nl</a><br>
+    </div>
+    <div class="d-table-row">
+      <span class="d-table-cell">Email</span>
+      <a class="d-table-cell" href="mailto:info@crazydutchbikers.nl">info@crazydutchbikers.nl</a><br>
+    </div>
+    <div class="d-table-row">
+      <span class="d-table-cell pr-3">Telefoon</span>
+      <a class="d-table-cell" href="tel:0646326795">06 46326795</a>
+    </div>
+  </p>
+  <h3 class="h5">Persoonsgegevens die wij verwerken</h3>
+  <p>
+    Crazy Dutch Bikers verwerkt je persoonsgegevens doordat je gebruik maakt van onze diensten en/of omdat je deze
+    gegevens zelf aan ons verstrekt.
+    Hieronder vind je een overzicht van de persoonsgegevens die wij verwerken:
+    <ul>
+      <li>Persoonsgegevens die je actief verstrekt bijvoorbeeld door een profiel op deze website aan te maken, in
+        correspondentie en telefonisch</li>
+    </ul>
+  </p>
+  <h3 class="h5">Bijzondere en/of gevoelige persoonsgegevens die wij verwerken</h3>
+  <p>
+    Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan 16
+    jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan
+    16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te
+    voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als je er van overtuigd bent
+    dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met
+    ons op via info@crazydutchbikers.nl, dan verwijderen wij deze informatie.
+    Met welk doel en op basis van welke grondslag wij persoonsgegevens verwerken
+  </p>
+  <h3 class="h5">Doelen</h3>
+  <p>
+    Crazy Dutch Bikers verwerkt jouw persoonsgegevens voor de volgende doelen:
+    <ul>
+      <li>Je te kunnen bellen of e-mailen indien dit nodig is om onze dienstverlening uit te kunnen voeren</li>
+      <li>Je de mogelijkheid te bieden een account aan te maken</li>
+    </ul>
+  </p>
+  <h3 class="h5">Geautomatiseerde besluitvorming</h3>
+  <p>
+    Crazy Dutch Bikers neemt niet op basis van geautomatiseerde verwerkingen besluiten over zaken die (aanzienlijke)
+    gevolgen kunnen hebben voor personen.
+    Het gaat hier om besluiten die worden genomen door computerprogramma's of -systemen, zonder dat daar een mens
+    (bijvoorbeeld een medewerker van Crazy Dutch Bikers) tussen zit.
+  </p>
+  <h3 class="h5">Hoe lang we persoonsgegevens bewaren</h3>
+  <p>
+    Crazy Dutch Bikers bewaart je persoonsgegevens niet langer dan strikt nodig is om de doelen te realiseren waarvoor
+    je gegevens worden verzameld.
+  </p>
+  <h3 class="h5">Delen van persoonsgegevens met derden</h3>
+  <p>
+    Crazy Dutch Bikers verstrekt uitsluitend aan derden en alleen als dit nodig is voor de uitvoering van onze
+    overeenkomst met jou of om te voldoen aan een wettelijke verplichting.
+    Cookies, of vergelijkbare technieken, die wij gebruiken.
+  </p>
+  <p>
+    Crazy Dutch Bikers gebruikt alleen technische en functionele cookies. En analytische cookies die geen inbreuk maken
+    op je privacy. Een cookie is een klein tekstbestand dat bij het eerste bezoek aan deze website wordt opgeslagen op
+    jouw computer, tablet of smartphone. De cookies die wij gebruiken zijn noodzakelijk voor de technische werking van
+    de website en jouw gebruiksgemak. Ze zorgen ervoor dat de website naar behoren werkt en onthouden bijvoorbeeld jouw
+    voorkeursinstellingen. Ook kunnen wij hiermee onze website optimaliseren. Je kunt je afmelden voor cookies door je
+    internetbrowser zo in te stellen dat deze geen cookies meer opslaat. Daarnaast kun je ook alle informatie die eerder
+    is opgeslagen via de instellingen van je browser verwijderen.
+  </p>
+  <h3 class="h5">Gegevens inzien, aanpassen of verwijderen</h3>
+  <p>
+    Je hebt het recht om je persoonsgegevens in te zien, te corrigeren of te verwijderen. Daarnaast heb je het recht om
+    je eventuele toestemming voor de gegevensverwerking in te trekken of bezwaar te maken tegen de verwerking van jouw
+    persoonsgegevens door Crazy Dutch Bikers en heb je het recht op gegevensoverdraagbaarheid. Dat betekent dat je bij
+    ons een verzoek kan indienen om de persoonsgegevens die wij van jou beschikken in een computerbestand naar jou of
+    een ander, door jou genoemde organisatie, te sturen. Je kunt een verzoek tot inzage, correctie, verwijdering,
+    gegevensoverdraging van je persoonsgegevens of verzoek tot intrekking van je toestemming of bezwaar op de verwerking
+    van jouw persoonsgegevens sturen naar info@crazydutchbikers.nl. Om er zeker van te zijn dat het verzoek tot inzage
+    door jou is gedaan, vragen wij jou een kopie van je identiteitsbewijs met het verzoek mee te sturen. Maak in deze
+    kopie je pasfoto, MRZ (machine readable zone, de strook met nummers onderaan het paspoort), paspoortnummer en
+    Burgerservicenummer (BSN) zwart. Dit ter bescherming van je privacy. We reageren zo snel mogelijk, maar binnen vier
+    weken, op jouw verzoek . Crazy Dutch Bikers wil je er tevens op wijzen dat je de mogelijkheid hebt om een klacht in
+    te dienen bij de nationale toezichthouder, de Autoriteit Persoonsgegevens. Dat kan via de volgende link:
+    <a
+      href="https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons">https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons</a>.
+  </p>
+  <h3 class="h5">Hoe wij persoonsgegevens beveiligen</h3>
+  <p>
+    Crazy Dutch Bikers neemt de bescherming van jouw gegevens serieus en neemt passende maatregelen om misbruik,
+    verlies, onbevoegde toegang, ongewenste openbaarmaking en ongeoorloofde wijziging tegen te gaan. Als jij het idee
+    hebt dat jouw gegevens toch niet goed beveiligd zijn of er aanwijzingen zijn van misbruik, neem dan contact op met
+    onze klantenservice of via <a href="mailto:info@crazydutchbikers.nl">info@crazydutchbikers.nl</a>.
+  </p>
+</div>
+@endsection
