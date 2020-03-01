3d3548bc203e75458c5e3637ebebbf8bf639eafa
Laytan Laats <Laytanlaats@hotmail.com>
2020-03-01 21:58:38 +0100

    clean up applicationcreated event which uses no sockets

--- app/Events/ApplicationCreated.php
+++ app/Events/ApplicationCreated.php
@@ -3,15 +3,12 @@
 namespace App\Events;
 
 use App\Application;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class ApplicationCreated
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     public $application;
 
@@ -23,15 +20,5 @@ class ApplicationCreated
     public function __construct(Application $application)
     {
         $this->application = $application;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
