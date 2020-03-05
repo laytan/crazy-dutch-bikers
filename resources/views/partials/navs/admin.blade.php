d0147b65903f8d36140971063a1f0011f6ccd191
Laytan Laats <laytanlaats@hotmail.com>
2020-03-05 14:51:05 +0100

    make menu more responsive

--- resources/views/partials/navs/admin.blade.php
+++ resources/views/partials/navs/admin.blade.php
@@ -1,14 +1,14 @@
 @component('components.dropdown', ['id' => 'members-dropdown', 'title' => 'Leden'])
-	<a href="{{ route('users.create') }}" class="dropdown-item">Leden toevoegen</a>
-	<a href="{{ route('users.index') }}" class="dropdown-item">Leden overzicht</a>
+	<a href="{{ route('users.create') }}" class="dropdown-item">Toevoegen</a>
+	<a href="{{ route('users.index') }}" class="dropdown-item">leden</a>
 @endcomponent
 @component('components.dropdown', ['id' => 'events-dropdown', 'title' => 'Evenementen'])
-	<a href="{{ route('events.create') }}" class="dropdown-item">Evenementen toevoegen</a>
-	<a href="{{ route('events.index') }}" class="dropdown-item">Evenementen overzicht</a>
+	<a href="{{ route('events.create') }}" class="dropdown-item">Toevoegen</a>
+	<a href="{{ route('events.index') }}" class="dropdown-item">Evenementen</a>
 @endcomponent
 @component('components.dropdown', ['id' => 'merchandise-dropdown', 'title' => 'Merchandise'])
-	<a href="{{ route('products.create') }}" class="dropdown-item">Merchandise toevoegen</a>
-	<a href="{{ route('products.index') }}" class="dropdown-item">Merchandise overzicht</a>
+	<a href="{{ route('products.create') }}" class="dropdown-item">Toevoegen</a>
+	<a href="{{ route('products.index') }}" class="dropdown-item">Merchandise</a>
 @endcomponent
 <li class="nav-item">
 	<a href="{{ route('galleries.index') }}" class="nav-link">
