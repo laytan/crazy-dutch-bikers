36ee4c82fa9e41db5ae107f7fbc640e7ff3239fb
Laytan Laats <laytanlaats@hotmail.com>
2020-03-13 23:30:01 +0100

    add editing to pictures on front-end

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -24,6 +24,7 @@
           </div>
         @endcan
     </div>
+    {{ Aire::summary()->verbose() }}
     <div class="gallery-grid">
         @foreach($gallery->picture_columns as $column)
         <div class="gallery-grid__column">
@@ -32,11 +33,34 @@
                 <img class="lazy" data-src="{{ Storage::url($picture->url) }}">
                 <div class="gallery-grid__icon-bar">
                   @can('manage')
-                    <button class="btn btn-warning btn-sm">
-                      <i class="fa fa-trash" data-submit="#destroy-{{ $picture->id }}"></i>
+                    <button class="btn btn-warning btn-sm" data-toggle="modal" data-target="#destroy-{{ $picture->id }}-modal">
+                      <i class="fa fa-trash"></i>
                     </button>
-                    {{ Aire::open()->route('pictures.destroy', ['picture' => $picture->id])->id("destroy-$picture->id")->class('d-none') }}
-                    {{ Aire::close() }}
+                    @component('components.modal', ['title' => 'Foto verwijderen?', 'id' => 'destroy-' . $picture->id . '-modal'])
+                      Weet je zeker dat deze foto verwijderd moet worden?
+                      {{ Aire::open()->route('pictures.destroy', ['picture' => $picture->id])->id("destroy-$picture->id")->class('d-none') }}
+                      {{ Aire::close() }}
+                      @slot('footer')
+                        <button class="btn btn-warning" data-submit="#destroy-{{ $picture->id }}">Verwijderen</button>
+                      @endslot
+                    @endcomponent
+                    <button class="btn btn-secondary btn-sm" data-toggle="modal" data-target="#edit-{{ $picture->id }}-modal">
+                      <i class="fas fa-edit"></i>
+                    </button>
+                    @component('components.modal', ['title' => 'Foto bewerken', 'id' => 'edit-' . $picture->id . '-modal'])
+                      {{ Aire::open()
+                        ->route('pictures.update', ['picture' => $picture->id])
+                        ->class('bg-transparant')
+                        ->bind($picture)
+                        ->id('picture-' . $picture->id . '-update-form')
+                      }}
+                      {{ Aire::checkbox('is_private', 'Prive?') }}
+                      {{ Aire::checkbox('is_featured', 'Uitgelicht?') }}
+                      {{ Aire::close() }}
+                      @slot('footer')
+                        <button class="btn btn-primary" data-submit="#picture-{{ $picture->id }}-update-form">Bijwerken</button>
+                      @endslot
+                    @endcomponent
                   @endcan
                   <button class="btn btn-primary btn-sm">
                     <i class="fa fa-close"></i>
